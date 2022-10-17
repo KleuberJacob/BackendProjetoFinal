6c2a010df481d82cde3faef4ab4d89f9c203310a
Kleuber de Paula Jacob <00787744@araujo.corp>
2022-10-17 18:12:05 -0300

    Create MD5 hash

--- src/main/java/br/com/xmarket/Controller/UsuarioController.java
+++ src/main/java/br/com/xmarket/Controller/UsuarioController.java
@@ -1,5 +1,9 @@
 package br.com.xmarket.Controller;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +18,17 @@ import br.com.xmarket.Model.Usuario;
 @RestController
 @RequestMapping("/cadastro")
 public class UsuarioController {
+	
+	public static String getHashMd5(String value) {
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        BigInteger hash = new BigInteger(1, md.digest(value.getBytes()));
+        return hash.toString(16);
+    }
 
 	@Autowired
 	private UsuarioDao dao;
@@ -23,8 +38,12 @@ public class UsuarioController {
 	public @ResponseBody Usuario novoUsuario(@RequestBody Usuario usuario) {
 		
 		System.out.println(usuario.getSenha_usuario());
+		String hash = getHashMd5(usuario.getSenha_usuario());
+		
+		usuario.setSenha_usuario(hash);		
+		
 		dao.save(usuario);
-		System.out.println(usuario.toString()); 
+//		System.out.println(usuario.toString()); 
 		return usuario;
 	}
 	
