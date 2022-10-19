a6d76b7d13bb01e5da05bbf25134a24a8c73be22
Kleuber de Paula Jacob <00787744@araujo.corp>
2022-10-19 09:14:26 -0300

    Login/Cadastro validation email/password

--- src/main/java/br/com/xmarket/Controller/UsuarioController.java
+++ src/main/java/br/com/xmarket/Controller/UsuarioController.java
@@ -13,6 +13,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -45,32 +46,41 @@ public class UsuarioController {
 	@CrossOrigin
 	@RequestMapping("/cadastro")
 	@PostMapping
-	public @ResponseBody Usuario novoUsuario(@RequestBody Usuario usuario) {		
+	public @ResponseBody ResponseEntity<Boolean> novoUsuario(@RequestBody Usuario usuario) {		
 		
-		String hash = getHashMd5(usuario.getSenha_usuario());
-		
-		usuario.setSenha_usuario(hash);		
-		
-		dao.save(usuario);
-		return usuario;
+		if(validarEmail(usuario.getEmail_usuario())==false) {
+			String hash = getHashMd5(usuario.getSenha_usuario());
+			usuario.setSenha_usuario(hash);		
+			dao.save(usuario);
+			return ResponseEntity.ok(true);		
+		}else {
+			System.out.println("false");
+			return ResponseEntity.ok(false);	
+		}	
 	}
 	
 	@CrossOrigin
 	@RequestMapping("/login")
 	@PostMapping
-	public @ResponseBody String validaUsuario(@RequestBody String usuario) throws ParseException {	
-		Object ob = new JSONParser().parse(usuario);
-		
-		JSONObject json = (JSONObject) ob;
+	public @ResponseBody ResponseEntity<Boolean> validaUsuario(@RequestBody String usuario) throws ParseException {	
+		Boolean logado=false;
+		String senha=null;
+		JSONObject json = (JSONObject) new JSONParser().parse(usuario);
 		String email= (String) json.get("tLogin");
-		String senha= getHashMd5((String) json.get("tPassword"));
-		
-		System.out.println(senha);
-		System.out.println(email);
-		System.out.println(validarEmail(email));
-		
-
-		return email;
+		if(validarEmail(email)==true) {
+			senha= getHashMd5((String) json.get("tPassword"));
+			logado = validarSenha(email, senha);	
+			System.out.println(senha);
+			System.out.println(email);
+			System.out.println(validarEmail(email));
+			System.out.println(logado);
+			if(logado == true) {
+				return ResponseEntity.ok(true);				
+			}else {
+				return ResponseEntity.ok(false);
+			}
+		}
+		return null;
 	}
 	
 	public Boolean validarEmail(String email) {
@@ -82,8 +92,23 @@ public class UsuarioController {
 				result=true;
 			}
 		}
+		return result;		
+	}
+	
+	public Boolean validarSenha(String email, String senha) {
+		Boolean result=false;
+		ArrayList<Usuario> lista= (ArrayList<Usuario>)(dao.findAll());
+
+		for(int i=0; i<lista.size();i++) {
+			if(lista.get(i).getEmail_usuario().equals(email)) {
+				if (lista.get(i).getSenha_usuario().equals(senha)){
+					result=true;
+				}else {
+					result=false;
+				}
+			}
+		}
 		return result;
-		
 	}
 	
 	
